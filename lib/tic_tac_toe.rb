e4f651cc685923d3ac87c10404a8237f2b6f40a2
kvnlay <kingstevens007@gmail.com>
2019-03-25 15:29:00 +0100

    added reset game method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,14 +2,15 @@
 class TicTacToe
   attr_accessor :board
   @@turn = 0
-  
+  @@char_play = ""
+
   WINNING_COMBINATION = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
 
   [0, 3, 6],
-  [1, 2, 7],
+  [1, 4, 7],
   [2, 5, 8],
 
   [0, 4, 8],
@@ -41,6 +42,7 @@ class TicTacToe
 
   def end_game
   puts "Thank you for playing TIC_TAC_TOE"
+  exit
   end
 
   def ask_player
@@ -53,16 +55,16 @@ class TicTacToe
   end
 
   def edit_board
-    if @@turn % 2 == 0
-      char_play = 'X'
+    if @@turn % 2 == 0 
+       @@char_play = "X"
     elsif
-      char_play = 'O'
+      @@char_play = "O"
     end
-    @board[@position] = char_play
+    @board[@position] = @@char_play
     display_board
     @@turn += 1
-    #return self.winner_player if game_won?
-    p  game_won? 
+    winner_player(@@char_play) if game_won? 
+    return is_draw if game_full? && @@turn == 8
   end
 
 
@@ -72,20 +74,52 @@ class TicTacToe
       first_index = item[0]
       second_index = item[1]
       third_index = item[2]
-      return true if @board[first_index] == @board[second_index] && @board[second_index] == @board[third_index] && @board[first_index] != '_'
+
+      if @board[first_index] == @board[second_index] && @board[second_index] == @board[third_index] && @board[first_index] != '_'
+        return true 
+      end
     end
     return false
   end
 
+  def winner_player(index)
+    if index == 'X'
+      puts "player 1 wins"
+    else
+      puts "player 2 wins"
+    end
+    reset_game
+
+  end
+
+  def reset_game
+    puts "Do you want to reset the game?"
+    puts "Y/N"
+    answer = gets.chomp.downcase
+    if answer == 'y'
+      @@turn = 0
+      @board = Array.new(9, '_')
+      play
+    else
+      end_game
+    end
+  end
+
+  def play
+    while @@turn < 9
+      ask_player
+      edit_board
+    end
+  end
+
+  def game_full?
+    return true if @board.none?("_")
+  end
+
+  def is_draw
+    puts "The game is a draw"
+  end
 end
 
 new_game = TicTacToe.new
-new_game.welcome
-
-
-while new_game.turn < 9
-  new_game.ask_player
-  new_game.edit_board
-end
-
-new_game.end_game
+new_game.play
